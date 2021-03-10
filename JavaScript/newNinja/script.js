5c9cd1e2031990be9644259e908e047c5c5caf59
Tom Wexler <wexler18@gmail.com>
2021-03-10 10:14:07 -0500

    extend

---
+++ JavaScript/newNinja/script.js
@@ -0,0 +1,43 @@
+class ninja{
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log("Hi! My name is "+ this.name+"!");
+    }
+    showStats(){
+        console.log("Name: "+this.name, "| Health: "+this.health, "| Speed: "+this.speed, "| Strength: "+this.strength);
+    }
+
+    drinkSake(){
+        this.health += 10;
+        let oldHealth = this.health - 10;
+        console.log("Plus-ten health! " + oldHealth + " -> " + this.health);
+    }
+}
+
+
+class sensei extends ninja{
+    constructor(name){
+        super(name, 200);
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("I am wise!");
+    }
+    showStats(){
+        super.showStats();
+        //console.log(yo+ "Wisdom: " +this.wisdom);   didn't work... asking question. log below
+    }
+}
+
+//Roshi.showStats();
+// Name: Roshi | Health: 200 | Speed: 10 | Strength: 10 script.js:12:17
+// undefinedWisdom: 10
+
