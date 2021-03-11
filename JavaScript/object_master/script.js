84635179619d3c47df8e1272a626ef4399a7f6bc
Tom Wexler <wexler18@gmail.com>
2021-03-11 12:48:28 -0500

    pokemon map

---
+++ JavaScript/object_master/script.js
@@ -0,0 +1,48 @@
+const pokémon = Object.freeze([
+    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+    { "id": 98,  "name": "Krabby",     "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+    { "id": 133, "name": "Eevee",      "types": ["normal"] },
+    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+]);
+
+
+const divisThree = pokémon.filter(a => a.id % 3 === 0);
+
+const fire = pokémon.filter(a => a.types.includes("fire"));
+
+const multTypes = pokémon.filter(a => a.types.length > 1);
+
+const justNames = pokémon.map(a => a.name);
+
+const above = pokémon.filter(a => a.id > 99).map(a => a.name); //filter before map
+
+// const poison = pokémon.filter(a => a.types == 1 && a.types.includes("poison")).map(a => a.name); 
+// doesn't work, but I want the way to do this simpler.
+
+const poison = pokémon.filter(a => a.types.length == 1).filter(a => a.types.includes("poison")).map(a => a.name); //filter before map
+
+const flying = pokémon.filter(a => a.types[1] == "flying").map(a => a.types[0]);
+
+// const flying = pokémon.filter(a => a.types[1].includes("flying"));
+//why doesn't this work???
+
+const normal = pokémon.filter(a => a.types.includes("normal")).length;
